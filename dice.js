7a1c6301c0f171746160098e319ee8ad8bb0e2ad
Brandon Wong <b.lh.wong@utexas.edu>
2016-12-27 22:12:02 -0600

    completed functionality of dice game

--- dice.js
+++ dice.js
@@ -1,15 +1,30 @@
-var filled = [false, false, false, false, false, false];
-var playerDrinkCount = [0,0,0,0,0,0]
+var filled = [0, 0, 0, 0, 0, 0];
+var playerDrinkCount = [];
 var players = prompt("How many players?");
 var rounds = prompt("How many rounds?");
 
+for(var j = 0; j < players; j++){
+  playerDrinkCount.push(0);
+}
+console.log(playerDrinkCount);
 console.log(players, rounds);
 playerIndex = 0;
 for(var i = 0; i < rounds; i++){
-  while(!filled[Math.floor(Math.random() * 6)]){
+  var rolled = Math.floor(Math.random() * 6);
+  //console.log('player ', playerIndex, ' rolled: ', rolled);
+  while(filled[rolled]){
+    //console.log("drink");
     playerDrinkCount[playerIndex] += 1;
+    //console.log('player drink: ', playerDrinkCount);
+    filled[rolled] ^= 1;
+    //console.log('filled: ', filled);
+    rolled = Math.floor(Math.random() * 6);
+    //console.log('player ', playerIndex, ' rolled: ', rolled);
   }
-  playerIndex = (playerIndex + 1) % 6;
+  //console.log('fill');
+  playerIndex = (playerIndex + 1) % players;
+  filled[rolled] ^= 1;
+  //console.log('filled: ', filled);
 }
 
-console.log(playerDrinkCount);
+console.log('end player drink: ', playerDrinkCount);
